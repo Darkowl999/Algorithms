ad3350c461fcad6549c639e601087203cc9d4a82
Darkowl999 <raulalbertoortiz290199@hotmail.com>
2021-06-10 17:21:40 -0400

    añadiendo nuevos archivos

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,27 @@
+import java.util.Stack;
+
 public class Prueba {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo cruel");
+    } 
+    
+}
+
+class Vehiculos{
+    private int numerodePuertas;
+    private int numerodeRuedas;
+    private String color;
+
+    Vehiculos(int numerodePuert,int numerodeRuedas,String color){
+        this.numerodePuertas=numerodePuert;
+        this.numerodeRuedas=numerodeRuedas;
+        this.color=color;
     }
 
-
-
 }
+
+
+
+
+
